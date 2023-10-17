1d80fa36843d30ec27327399fc431478bb8cb09e
rozhanalvandi <129389370+rozhanalvandi@users.noreply.github.com>
2023-10-17 23:16:08 +0330

    upload file

---
+++ Data/ResumeDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Resume.Presentation.Models.Entitis.Education;
+using Resume.Presentation.Models.Entitis.Exprience;
+using Resume.Presentation.Models.Entitis.MySkills;
+
+namespace Resume.Presentation.Data
+{
+	public class ResumeDbContext : DbContext
+	{
+		public ResumeDbContext(DbContextOptions<ResumeDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Exprience> Expriences { get; set; }
+        public DbSet<MySkills> MySkills { get; set; }
+
+    }
+}
+
