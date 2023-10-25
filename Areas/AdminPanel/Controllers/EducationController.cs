1c0f68ec3a8edef40b18617a0efaab5d827ea348
rozhanalvandi <129389370+rozhanalvandi@users.noreply.github.com>
2023-10-25 13:11:31 +0330

    Add Education & MySkills to Adminpanel

---
+++ Areas/AdminPanel/Controllers/EducationController.cs
@@ -0,0 +1,64 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Resume.Application.Services.Implementation;
+using Resume.Application.Services.Interfaces;
+using Resume.Domain.Entities.Education;
+using Resume.Domain.Entities.MySkills;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Resume.Presentation.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class EducationController : Controller
+    {
+        private readonly IEducationService _educationService;
+        public EducationController(IEducationService educationService)
+        {
+            _educationService = educationService;
+
+        }
+        public async Task<IActionResult> ListOfAnEducation()
+        {
+            var model = await _educationService.GetListOfEducations();
+            return View(model);
+        }
+        public async Task<IActionResult> CreateAnEducation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAnEducation(Education education)
+        {
+            await _educationService.AddAnEducationToDatabase(education);
+            return RedirectToAction(nameof(ListOfAnEducation));
+        }
+        public async Task<IActionResult> EditAnEducation(int educationId)
+        {
+            var education = await _educationService.GetEducationById(educationId);
+            return View(education);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditAnEducation(Education education)
+        {
+            await _educationService.AddAnEducationToDatabase(education);
+            return RedirectToAction(nameof(ListOfAnEducation));
+
+        }
+        public async Task<IActionResult> DeleteAnEducation(int educationId)
+        {
+            var education = await _educationService.GetEducationById(educationId);
+            return View(education);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteAnEducation(Education education)
+        {
+            await _educationService.DeleteAnEducation(education);
+            return RedirectToAction(nameof(ListOfAnEducation));
+
+        }
+    }
+}
---
+++ Areas/AdminPanel/Controllers/MySkillsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Resume.Application.Services.Interfaces;
+using Resume.Domain.Entities.MySkills;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Resume.Presentation.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class MySkillsController : Controller
+    {
+        public MySkillsController(IMyskillsService myskillsService)
+        {
+            _myskillsService = myskillsService;
+        }
+
+        public IMyskillsService _myskillsService { get; }
+
+        public async Task<IActionResult> ListOfMySkills()
+        {
+            var skills = await _myskillsService.GetListOfMySkills();
+            return View(skills);
+        }
+        public async Task<IActionResult> CreateMySkills()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateMySkills(MySkills mySkills)
+        {
+            await _myskillsService.AddMySkillSToDataBase(mySkills);
+            return RedirectToAction(nameof(ListOfMySkills));
+
+        }
+        public async Task<IActionResult> EditMySkills(int myskillsId)
+        {
+            var myskill = await _myskillsService.GetMySkillsById(myskillsId);
+            return View(myskill);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditMySkills(MySkills mySkills)
+        {
+            await _myskillsService.EditMYSkills(mySkills);
+            return RedirectToAction(nameof(ListOfMySkills));
+
+        }
+        public async Task<IActionResult> DeleteMySkills(int myskillsId)
+        {
+            var myskill = await _myskillsService.GetMySkillsById(myskillsId);
+            return View(myskill);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteMySkills(MySkills mySkills)
+        {
+            await _myskillsService.DeleteMYSkills(mySkills);
+            return RedirectToAction(nameof(ListOfMySkills));
+            return View(mySkills);
+
+        }
+    }
+}
